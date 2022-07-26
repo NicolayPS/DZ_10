0fca378651107310eb18af85085c05d0f35f3934
oksanaPC2 <77opod@gmail.com>
2022-07-26 20:45:41 +0300

    Добавьте файлы проекта.

---
+++ DZ_10/AngularSecond.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_10
+{
+    class AngularSecond
+    {
+        double gradus;//ввод данных 
+        double min;
+        double sec;
+        public bool isCorrect = true;
+
+        public double Gradus //проверка градусов
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    gradus = value % 360;
+            }
+        }
+        public double Min //проверка минут
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+        public double Sec //проверка секунд
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+
+        public AngularSecond(double gradus, double min, double sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return (gradus + (min + sec / 60) / 60)*Math.PI/180;
+        }
+    }
+}
---
+++ DZ_10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AngularSecond angularsecond;
+            do
+            {
+                Console.Write("Введите данные\nВведите градусы:");
+                double gradus_ = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите минуты:");
+                double min_ = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите секунды:");
+                double sec_ = Convert.ToDouble(Console.ReadLine());
+
+                angularsecond = new AngularSecond(gradus_, min_, sec_);
+            }
+            while (angularsecond.isCorrect == false);
+
+            double totalRadians = angularsecond.ToRadians();
+            Console.WriteLine(totalRadians);
+            Console.ReadKey();
+        }
+    }
+}
